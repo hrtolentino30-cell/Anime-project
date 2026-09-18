e61cd080ffda0267d6427b7fac833d7d1c099841
hrtolentino30-cell <hrtolentino30@gmail.com>
2026-09-18 13:02:40 +0800

    seo: use animori.bond canonical domain

--- app/robots.ts
+++ app/robots.ts
@@ -1,2 +1,2 @@
 import type { MetadataRoute } from 'next';
-export default function robots():MetadataRoute.Robots{return{rules:[{userAgent:'*',allow:'/',disallow:['/api/','/auth/','/history','/my-list']}],sitemap:'https://animori.vercel.app/sitemap.xml',host:'https://animori.vercel.app'}}
+export default function robots():MetadataRoute.Robots{return{rules:[{userAgent:'*',allow:'/',disallow:['/api/','/auth/','/history','/my-list']}],sitemap:'https://animori.bond/sitemap.xml',host:'https://animori.bond'}}
