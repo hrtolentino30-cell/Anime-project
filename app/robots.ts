48c9aea988a3e821b1010b469e6fc7ea20f5a81d
hrtolentino30-cell <hrtolentino30@gmail.com>
2026-09-18 14:02:12 +0800

    seo: block admin routes from crawlers

--- app/robots.ts
+++ app/robots.ts
@@ -1,2 +1,2 @@
 import type { MetadataRoute } from 'next';
-export default function robots():MetadataRoute.Robots{return{rules:[{userAgent:'*',allow:'/',disallow:['/api/','/auth/','/history','/my-list']}],sitemap:'https://www.animori.bond/sitemap.xml',host:'https://www.animori.bond'}}
+export default function robots():MetadataRoute.Robots{return{rules:[{userAgent:'*',allow:'/',disallow:['/api/','/auth/','/history','/my-list','/admin']}],sitemap:'https://www.animori.bond/sitemap.xml',host:'https://www.animori.bond'}}
